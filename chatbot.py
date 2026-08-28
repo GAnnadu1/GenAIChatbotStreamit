138e169edf51145f890c6d0959d42c728fb8faaf
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-08-28 10:21:45 +0200

    Implement basic chatbot using Streamlit and Langchain

---
+++ chatbot.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_openai import ChatOpenAI
+
+#load the env variables
+load_dotenv()
+
+#streamlit page setup
+st.set_page_config(
+    page_title='Chatbot',
+    page_icon='🤖',
+    layout='centered'
+)
+
+st.title('💬 Generative AI Chatbot')
+
+#initiate chat history
+#chat_history = []
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+#show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+#llm initiate
+llm = ChatOpenAI(model="claude-haiku-4-5-20251001", temperature=0)
+
+user_prompt = st.chat_input('Ask Chatbot...')
+
+#user input
+if user_prompt :
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user", "content":user_prompt})
+
+    response = llm.invoke(
+        input ["role": "system", "content": "You are a helpful assistant.", *st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role":"assistant", "content":assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
