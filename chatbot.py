f155037051a8dedfec80f069dee8a8527d7a4c21
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-08-28 10:34:19 +0200

    Refactor input formatting for llm.invoke

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,5 @@
+%%writefile chatbot.py
+
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_openai import ChatOpenAI
@@ -35,10 +37,7 @@ if user_prompt :
     st.session_state.chat_history.append({"role":"user", "content":user_prompt})
 
     response = llm.invoke(
-        input = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            *st.session_state.chat_history
-        ]
+        input = [{"role": "system", "content": "You are a helpful assistant."},*st.session_state.chat_history]
     )
     assistant_response = response.content
     st.session_state.chat_history.append({"role":"assistant", "content":assistant_response})
